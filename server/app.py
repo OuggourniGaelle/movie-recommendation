fb0c1380d6588004fd558b27b106946e57961e61
OuggourniGaelle <gaelle.ouggourni@gmail.com>
2017-11-06 17:51:37 +0100

    Premieres modif

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,8 @@ def hello_world():
 @app.route('/my-movies')
 def list_user_movies():
     return render_template('my-movies.html', page_title='My Movies')
+
+my_movies_rated = pd.read_csv('./data/my-ratings.csv', sep=',', header=0)
+
+
+{% for index, movie in my_movies_rated.iterrows() %}
